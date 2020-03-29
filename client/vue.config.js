d0fd665828a49eff857ba7cd5db9efd983d99600
cyoq <di9akoz@gmail.com>
2020-03-29 16:07:12 +0300

    adding base for creating frontend part

---
+++ client/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    pages: {
+        'auth': {
+            entry: './src/pages/Auth/main.js',
+            template: './public/index.html',
+            title: 'Authorization',
+            chunks: ['chunk-vendors', 'chunk-common', 'auth']
+        },
+        'index': {
+            entry: './src/pages/Home/main.js',
+            template: './public/index.html',
+            title: 'Home',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
+    }
+}
