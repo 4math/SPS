1f8a4d0096e8bc12f5a0ebbbca32bfceaa0fdf10
cyoq <di9akoz@gmail.com>
2020-04-10 17:57:58 +0300

    changed some structure

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <!-- <h1>Welcome to the home page!</h1> -->
+        <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+        <Login/>
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./components/auth/Login";
+
+export default {
+    name: "App",
+    components: {
+        // HelloWorld,
+        Login
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ client/src/components/auth/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="login-form">
+        <h1>
+            <b>Welcome to the Login Page!</b>
+        </h1>
+
+        <form action="#" @submit.prevent="login">
+
+            <div class="form-input">
+                <label for="email">E-mail:</label>
+                <br />
+                <input type="email" name="email" id="email" class="text-input" v-model="email" />
+            </div>
+
+            <div class="form-input">
+                <label for="password">Password:</label>
+                <br />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    class="text-input"
+                    v-model="password"
+                />
+            </div>
+
+            <div class="form-input">
+                <button class="submit-btn" type="submit">Submit</button>
+            </div>
+
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            email: "",
+            password: ""
+        };
+    },
+    methods: {
+      login() {
+        this.$store.dispatch('retrieveToken');
+      }
+    }
+};
+</script>
+
+<style>
+#login-form {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+input {
+    border-radius: 5px;
+    margin: 10px;
+}
+
+label {
+    align-content: flex-start;
+}
+</style>
