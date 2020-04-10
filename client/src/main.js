020efeae399ab53f1b28d48392ed3cd1f1bd1e45
cyoq <di9akoz@gmail.com>
2020-04-10 19:01:31 +0300

    vuex init

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router';
 import Router from 'vue-router';
-import Vuex from 'vuex';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -15,7 +14,6 @@ Vue.use(IconsPlugin);
 
 Vue.use(Router);
 
-Vue.use(Vuex);
 
 
 new Vue({
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,8 @@
-import Vuex from 'vuex';
+import Vue from 'vue';
 import auth from './modules/auth';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
