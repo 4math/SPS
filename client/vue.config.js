d6432993e4f9f37ea0ec51021aa63e916a6ac106
cyoq <di9akoz@gmail.com>
2020-04-11 15:50:31 +0300

    config files and libraries

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,10 @@
 module.exports = {
-    // pages: {
-    //     'auth': {
-    //         entry: './src/pages/Auth/main.js',
-    //         template: './public/index.html',
-    //         title: 'Authorization',
-    //         chunks: ['chunk-vendors', 'chunk-common', 'auth']
-    //     },
-    //     'index': {
-    //         entry: './src/pages/Home/main.js',
-    //         template: './public/index.html',
-    //         title: 'Home',
-    //         chunks: ['chunk-vendors', 'chunk-common', 'index']
-    //     }
-    // }
+    runtimeCompiler: true,
+    css: {
+        loaderOptions: {
+            scss: {
+                // prependData: `@import "~@/scss/_variables.scss";`
+            }
+        }
+    }
 }
