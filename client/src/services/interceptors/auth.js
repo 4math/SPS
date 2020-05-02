69bdab60cb59d69027bf4554d4875dfcbc7ea6cb
cyoq <di9akoz@gmail.com>
2020-05-02 15:22:10 +0300

    refresh jwt token on client side

--- client/src/services/interceptors/auth.js
+++ client/src/services/interceptors/auth.js
@@ -1,31 +1,39 @@
-
 import { AUTH_LOGOUT } from "@/store/actions/auth";
-import store from '@/store';
+import store from "@/store";
 
 export default function setup(http) {
+  http.interceptors.request.use(
+    config => {
+      const token = localStorage.getItem("user-token");
+      if (token) {
+        config.headers.common["Authorization"] = "Bearer " + token;
+      }
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
 
-    http.interceptors.request.use(config => {
+  http.interceptors.response.use(
+    response => {
+      const newToken = response.headers.authorization;
+      if (newToken) {
+        // using substr to delete 'Bearer ' 
+        localStorage.setItem("user-token", newToken.substr(7));
+      }
 
-        const token = localStorage.getItem('user-token');
-        if (token) {
-            config.headers.common['Authorization'] = "Bearer " + token;
-        }
-        return config;
-    }, error => {
-        return Promise.reject(error);
-    });
-
-    http.interceptors.response.use(
-        response => {
-            return response;
-        },
-        err => {
-            if (err.status === 401) {
-                // if you ever get an unauthorized, logout the user
-                store.dispatch(AUTH_LOGOUT);
-            }
-            return Promise.reject(err);
-        }
-    );
-
+      if (response.status === 401) {
+        // if you ever get an unauthorized, logout the user
+        store.dispatch(AUTH_LOGOUT);
+      }
+      return response;
+    },
+    error => {
+      if (error.response && error.response.data) {
+        return Promise.reject(error.response.data);
+      }
+      return Promise.reject(error.message);
+    }
+  );
 }
