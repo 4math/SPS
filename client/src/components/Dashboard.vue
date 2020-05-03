bd4c991b8018632876b565eed4b406fa8593c568
cyoq <di9akoz@gmail.com>
2020-05-03 22:28:43 +0300

    added socket controls

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <h1>Welcome to the dashboard, {{ getProfile.name }}!</h1>
+    <Socket
+      v-for="socket in sockets"
+      :key="socket.id"
+      v-bind="socket"
+      :title="socket.name"
+      :description="socket.description"
+      :state="!!socket.switch_state"
+      @put="put"
+    />
   </div>
 </template>
 
@@ -9,38 +18,57 @@ import { mapGetters } from "vuex";
 import { USER_REQUEST } from "@/store/actions/user";
 import store from "@/store";
 import router from "@/router/router";
+import Socket from "@/components/Socket.vue";
 
 export default {
-    name: "Dashboard",
-    data() {
-        return {};
-    },
-    computed: {
-        ...mapGetters(["getProfile"])
-    },
-    methods: {},
-    beforeRouteEnter(to, from, next) {
-        if (store.getters.isAuthenticated) {
-            store
-                .dispatch(USER_REQUEST)
-                .then(() => {
-                    next();
-                })
-                .catch(() => {
-                    router.push("/login").catch(console.error);
-                });
-        }
+  name: "Dashboard",
+  components: {
+    Socket,
+  },
+  data() {
+    return {
+      sockets: [],
+    };
+  },
+  computed: {
+    ...mapGetters(["getProfile"]),
+  },
+  created() {
+    this.axios
+      .get("/sockets/list")
+      .then((response) => {
+        this.sockets = response.data;
+      })
+      .catch(console.error);
+  },
+  methods: {
+    async put(id, value) {
+      await this.axios.put(`/sockets/${id}`, { state: +value});
+      this.sockets.find(socket => socket.id === id).state = value;
     }
+  },
+  beforeRouteEnter(to, from, next) {
+    if (store.getters.isAuthenticated) {
+      store
+        .dispatch(USER_REQUEST)
+        .then(() => {
+          next();
+        })
+        .catch(() => {
+          router.push("/login").catch(console.error);
+        });
+    }
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ client/src/components/Socket.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="socket-card">
+    <b-card
+      :title="title"
+      :img-src="image"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{ description }}
+      </b-card-text>
+
+      <b-input-group id="switch" size="lg">
+        <b-input-group-prepend is-text>
+          <b-form-checkbox
+            switch
+            class="mr-n2"
+            :checked="state"
+            @change="put"
+          />
+        </b-input-group-prepend>
+      </b-input-group>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Socket",
+  props: {
+    id: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    title: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    description: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    state: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  computed: {
+    image() {
+      const image = this.state ? "bulb_on" : "bulb";
+      // eslint-disable-next-line no-undef
+      return require(`@/assets/images/${image}.jpg`);
+    },
+  },
+  methods: {
+    put(value) {
+      this.$emit("put", this.id, value);
+    }
+  },
+};
+</script>
+
+<style scoped>
+#socket-card {
+  display: inline-block;
+  margin: 10px;
+}
+
+#switch {
+  margin: 0 auto;
+  display: inline;
+}
+</style>
