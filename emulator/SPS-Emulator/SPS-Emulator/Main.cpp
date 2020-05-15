9e05471f359758a703f5b86441e2e4f213af07ec
Kirill Trofimov <kirilltrofimov@inbox.lv>
2020-05-15 14:37:56 +0300

    Added more usefulness

--- emulator/SPS-Emulator/SPS-Emulator/Main.cpp
+++ emulator/SPS-Emulator/SPS-Emulator/Main.cpp
@@ -9,14 +9,22 @@ using json = nlohmann::json;
 #include <chrono>
 #include <thread>
 
-const std::string unique_id("blablakek");
-
 struct Settings {
 	int state = 1;
 	long long report_delay = 1000;
 };
 
 int main() {
+	std::string unique_id("blablakek");
+
+	{
+		std::cout << "Default unique_id is 'blablakek', enter your unique_id to change or '-' leave it as is: ";
+		std::string temp_unique_id;
+		std::cin >> temp_unique_id;
+		if (temp_unique_id != "-")
+			unique_id = temp_unique_id;
+	}
+	
 	Settings settings;
 	Client client("127.0.0.1", 8000);
 
@@ -29,6 +37,10 @@ int main() {
 			};
 
 			auto response = client.Post("/api/sockets/add", payload.dump(-1), "application/json");
+			if (response && response->status != 200) {
+				std::cout << response->body << "\n";
+			}
+
 			socket_added = true;
 		}
 
@@ -40,11 +52,16 @@ int main() {
 
 			auto response = client.Post("/api/measurements/add", payload.dump(-1), "application/json");
 
-			if (response && response->status == 200) {
-				json response_payload = json::parse(response->body);
-				settings.state = response_payload["state"].get<int>();
+			if (response) { 
+				if (response->status == 200) {
+					json response_payload = json::parse(response->body);
+					settings.state = response_payload["state"].get<int>();
 
-				std::cout << (settings.state == 0 ? "disabled\n" : "enabled\n");
+					std::cout << (settings.state == 0 ? "disabled\n" : "enabled\n");
+				}
+				else {
+					std::cout << response->body << "\n";
+				}
 			}
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(settings.report_delay));
