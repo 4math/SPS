3fae2d3261b129836bddebc68ff9902b4e111c0a
cyoq <di9akoz@gmail.com>
2020-05-30 21:55:37 +0300

    logging system to the ws server

--- ws-server/src/utils/logger.ts
+++ ws-server/src/utils/logger.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
+import { Coloring } from "./colors";
 
-export default class Log {
+export default class Logger {
   private logger: fs.WriteStream;
 
   constructor() {
@@ -9,20 +10,33 @@ export default class Log {
     });
   }
 
-  /**
-   * info
-   */
   public info(data: string): void {
     const time: string = this.getTime();
-    const message: string = `[INFO] (${time}) - ${data}`;
-    this.logger.write(message + "\n");
-    console.log(message);
+    const prepend: string = "[INFO]";
+    const message: string = ` (${time}) - ${data}`;
+    this.logger.write(prepend + message + "\n");
+    console.log(Coloring.colorize(prepend, Coloring.Colors.fg.Green) + message);
+  }
+
+  public warn(data: string): void {
+    const time: string = this.getTime();
+    const prepend: string = "[WARNING]";
+    const message: string = ` (${time}) - ${data}`;
+    this.logger.write(prepend + message + "\n");
+    console.log(Coloring.colorize(prepend, Coloring.Colors.fg.Yellow) + message);
+  }
+
+  public error(data: string): void {
+    const time: string = this.getTime();
+    const prepend: string = "[ERROR]";
+    const message: string = ` (${time}) - ${data}`;
+    this.logger.write(prepend + message + "\n");
+    console.log(Coloring.colorize(prepend, Coloring.Colors.fg.Red) + message);
   }
 
   private getTime(): string {
-    //TODO set host's time
     let time: string = new Date().toUTCString();
-    time = time.split(' ').slice(0, 5).join(' ');
+    time = time.split(" ").slice(0, 5).join(" ");
     return time;
   }
 }
