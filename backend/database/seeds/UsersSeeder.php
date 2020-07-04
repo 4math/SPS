c917a4f2d096e39f0c9ee75da4e57bb37cea63a3
OlegGedzjuns <oleggedzjuns@gmail.com>
2020-07-04 14:46:34 +0300

    removed seeding

--- backend/database/seeds/UsersSeeder.php
+++ backend/database/seeds/UsersSeeder.php
@@ -9,18 +9,5 @@ class UsersSeeder extends Seeder
 {
     public function run(Faker $faker)
     {
-        $user = new User
-        ([
-            'name' => 'root',
-            'email' => 'root@example.net',
-            'password' => bcrypt('root'),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        $user->save();
-        //factory(User::class, 2)->create();
-        // factory(User::class, 2)->create()->each(function ($user){
-        //     $user->socket()->save()
-        // });
     }
 }
